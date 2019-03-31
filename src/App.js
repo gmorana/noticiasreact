73d25f2a02210fb9ffd35233310595a72c36b5e5
Jorge Morana <gmorana@gmail.com>
2019-03-31 14:12:52 -0500

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-
+import Header from './components/Header';
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<p>Noticias</p>
+				<Header titulo="Portal Noticias" />
 			</div>
 		);
 	}
